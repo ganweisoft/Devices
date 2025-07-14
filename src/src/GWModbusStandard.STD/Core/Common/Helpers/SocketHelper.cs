480eb562135ddf5de700093a03d04911f865750e
wangpeng <wangpeng@ganweisoft.com>
2025-07-14 18:00:47 +0800

    feat：update copyright

--- src/src/GWModbusStandard.STD/Core/Common/Helpers/SocketHelper.cs
+++ src/src/GWModbusStandard.STD/Core/Common/Helpers/SocketHelper.cs
@@ -1,4 +1,4 @@
-﻿//  Copyright (c) 2025 Shenzhen Ganwei Software Technology Co., Ltd
+﻿//  Copyright (c) 2021 Shenzhen Ganwei Software Technology Co., Ltd
 using System.Net.Sockets;
 
 namespace IoTClient.Common.Helpers

b0d00aafdfc157ea50244b0f763aa122f2570f6c
lihao <lihao@ganweisoft.com>
2025-08-06 13:40:27 +0800

    feat: add dapr simulate proj

---
+++ src/src/GWDapr.Subscribe.STD/CEquip.cs
@@ -0,0 +1,42 @@
+﻿using GWDataCenter;
+using GWDataCenter.Database;
+
+namespace GWDapr.Subscribe.STD
+{
+    public class CEquip : CEquipBase
+    {
+        /// <summary>
+        /// 是否初始化过
+        /// </summary>
+        private bool _isInit = false;
+
+        public override bool init(EquipItem item)
+        {
+            try
+            {
+                if (!base.init(item)) return false;
+                if (!_isInit || ResetFlag)
+                {
+                    _isInit = true;
+                }
+            }
+            catch (Exception e)
+            {
+                DataCenter.WriteLogFile($"初始化异常：{e}");
+                return false;
+            }
+
+            return _isInit;
+        }
+
+        public override bool GetYC(YcpTableRow r)
+        {
+            return true;
+        }
+
+        public override bool GetYX(YxpTableRow r)
+        {
+            return true;
+        }
+    }
+}
